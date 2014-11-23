3240bfa889614938971a82bd4d85924115aed298
Patrick Bellositz <e1027108@student.tuwien.ac.at>
2014-11-23 21:50:01 +0100

    [setup] can now open MainInput Window

--- bakk/src/main/Main.java
+++ bakk/src/main/Main.java
@@ -1,5 +1,33 @@
 package main;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
+public class Main extends Application{
+	static AnchorPane mainInput;
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+
+		try{
+			mainInput = FXMLLoader.load(getClass().getResource("/resources/MainInput.fxml"));
+		} catch (Exception e){
+			e.printStackTrace();
+			System.exit(-1);
+		}
+
+		Scene scene = new Scene(mainInput);
+		stage.setScene(scene);
+		stage.setTitle("Group Creation");
+		stage.setResizable(false);
+		stage.centerOnScreen();
+		stage.show();
+	}
 }
