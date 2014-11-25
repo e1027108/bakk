5c1b0c4007922270235fcd12c25568efa7944618
Patrick Bellositz <e1027108@student.tuwien.ac.at>
2014-11-25 23:50:55 +0100

    [0002] text input now gets saved into argument (mostly)

--- bakk/src/gui/MainInputController.java
+++ bakk/src/gui/MainInputController.java
@@ -41,8 +41,60 @@ public class MainInputController {
 		arguments = new ArrayList<ArgumentDto>();
 	}
 	
+	//TODO maybe change from A...J to A0...A9
 	@FXML
 	public void onShowButton(){
-		System.out.println("test");
+		if(!argumentATxt.getText().isEmpty() || !attackATxt.getText().isEmpty()){
+			arguments.add(new ArgumentDto('A', parseArgument(argumentATxt), parseAttacks(attackATxt)));
+		}
+		if(!argumentBTxt.getText().isEmpty() || !attackBTxt.getText().isEmpty()){
+			arguments.add(new ArgumentDto('B', parseArgument(argumentBTxt), parseAttacks(attackBTxt)));
+		}
+		if(!argumentCTxt.getText().isEmpty() || !attackCTxt.getText().isEmpty()){
+			arguments.add(new ArgumentDto('C', parseArgument(argumentCTxt), parseAttacks(attackCTxt)));
+		}
+		if(!argumentDTxt.getText().isEmpty() || !attackDTxt.getText().isEmpty()){
+			arguments.add(new ArgumentDto('D', parseArgument(argumentDTxt), parseAttacks(attackDTxt)));
+		}
+		if(!argumentETxt.getText().isEmpty() || !attackETxt.getText().isEmpty()){
+			arguments.add(new ArgumentDto('E', parseArgument(argumentETxt), parseAttacks(attackETxt)));
+		}
+		if(!argumentFTxt.getText().isEmpty() || !attackFTxt.getText().isEmpty()){
+			arguments.add(new ArgumentDto('F', parseArgument(argumentFTxt), parseAttacks(attackFTxt)));
+		}
+		if(!argumentGTxt.getText().isEmpty() || !attackGTxt.getText().isEmpty()){
+			arguments.add(new ArgumentDto('G', parseArgument(argumentGTxt), parseAttacks(attackGTxt)));
+		}
+		if(!argumentHTxt.getText().isEmpty() || !attackHTxt.getText().isEmpty()){
+			arguments.add(new ArgumentDto('H', parseArgument(argumentHTxt), parseAttacks(attackHTxt)));
+		}
+		if(!argumentITxt.getText().isEmpty() || !attackITxt.getText().isEmpty()){
+			arguments.add(new ArgumentDto('I', parseArgument(argumentITxt), parseAttacks(attackITxt)));
+		}
+		if(!argumentJTxt.getText().isEmpty() || !attackJTxt.getText().isEmpty()){
+			arguments.add(new ArgumentDto('J', parseArgument(argumentJTxt), parseAttacks(attackJTxt)));
+		}
+		
+		//TODO create graphwindow, send info there, open it (instead of maininput, but in the same window)
 	}
+	
+	private String parseArgument(TextField argument) {
+		if(argument.getText().isEmpty()){
+			return "no argument description";
+		}
+		return argument.getText();
+	}
+
+	private String parseAttacks(TextField attack) {
+		if(attack.getText().isEmpty()){
+			return "";
+		}
+		else{
+			String argumentNames = "ABCDEFGHIJ";
+			
+			//TODO remove everything like separators, but throw exception if there are invalid letters
+			return null; //TODO replace null with something
+		}
+	}
+
 }
