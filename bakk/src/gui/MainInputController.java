2672dc71cdcc76b965ff0a5084ced3cc3c4d1bdc
Patrick <Patrick@Patrick-PC>
2016-11-29 13:29:36 +0100

    slight changes

--- bakk/src/gui/MainInputController.java
+++ bakk/src/gui/MainInputController.java
@@ -17,7 +17,6 @@ import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
